7ed6c7df4020b89ecbaa7b596f8cf2a858b57e93
Nathan Hietala <Nrhietala@gmail.com>
2020-10-07 09:43:26 -0500

    Stuck on splicing

--- index.js
+++ index.js
@@ -146,11 +146,9 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(arr, flavName){
+
 }
-
-
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. 
